b7465014a8cb63ce4158e07d1b397e2ebea91068
Mostafa Elshaikh <m.elshaikh66@gmail.com>
2019-08-26 10:31:54 +0200

    subscribe our-work

--- js/custom.js
+++ js/custom.js
@@ -4,4 +4,22 @@ $(function() {
   $("html").niceScroll();
 
   // adjust header height
+  $(".header").height($(window).height());
+
+  // scroll to features
+
+  $(".header .arrow i").click(function() {
+    $("html, body").animate(
+      {
+        scrollTop: $(".feat").offset().top
+      },
+      1000
+    );
+  });
+
+  // show hidden items
+
+  $(".show-more").on("click", function() {
+    $(".our-work .hidden").fadeIn(1000);
+  });
 });
