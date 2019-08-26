d222a3d870416e1d32e26879a85e85fd2380a967
Mostafa Elshaikh <m.elshaikh66@gmail.com>
2019-08-26 22:53:10 +0200

    testimonial slider by jquery

--- js/custom.js
+++ js/custom.js
@@ -22,4 +22,38 @@ $(function() {
   $(".show-more").on("click", function() {
     $(".our-work .hidden").fadeIn(1000);
   });
+
+  // check testimonials
+  var rightArrow = $(".testim .fa-chevron-right"),
+    leftArrow = $(".testim .fa-chevron-left");
+  function checkClients() {
+    $(".client:first").hasClass("active")
+      ? leftArrow.fadeOut()
+      : leftArrow.fadeIn();
+    $(".client:last").hasClass("active")
+      ? rightArrow.fadeOut()
+      : rightArrow.fadeIn();
+  }
+  checkClients();
+  $(".testim i").click(function() {
+    if ($(this).hasClass("fa-chevron-right")) {
+      $(".testim .active").fadeOut(100, function() {
+        $(this)
+          .removeClass("active")
+          .next(".client")
+          .addClass("active")
+          .fadeIn();
+        checkClients();
+      });
+    } else {
+      $(".testim .active").fadeOut(100, function() {
+        $(this)
+          .removeClass("active")
+          .prev(".client")
+          .addClass("active")
+          .fadeIn();
+        checkClients();
+      });
+    }
+  });
 });
